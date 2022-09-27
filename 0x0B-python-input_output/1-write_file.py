a0a3ea7ef97ee3470cefe1d09d86f54ba8c016ec
Arnold Siyabonga Mdluli <arnoldmdluli00@gmail.com>
2022-09-27 21:46:33 +0000

    Task 1 fix 2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """"write file"""
     with open(filename, mode="w", encoding="utf-8") as file:
-         file.write(text)
+        file.write(text)
     return len(text)
