78a2f6f25cc644d45620e7b199583e8648824f4e
Arnold Mdluli <arnoldmdluli00@gmail.com>
2022-11-02 17:14:08 -0700

    Task 2 fix 1

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- Script creates user user_0d_2 and database hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create the user
-CREATE IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
--- grant privileges
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants SELECT privileges
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
 
