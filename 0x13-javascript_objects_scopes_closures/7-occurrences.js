8947a4940e7ab57b454cc60531b46a45aa2bab89
Arnold Siyabonga Mdluli <arnoldmdluli00@gmail.com>
2022-11-30 12:23:24 +0000

    Task 7 fix 2

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-//Function returns number of occurrences in a list
 exports.nbOccurences = function (list, searchElement) {
-  let ocurrences = 0;
-  list.forEach(element => {
-    if (element === searchElement) {
-      ocurrences++;
+  let nOccurrences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      nOccurrences++;
     }
-  });
-  return ocurrences;
+  }
+  return nOccurrences;
 };
