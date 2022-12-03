3a1d0f29583500eabf317cafb5c61911c6fb0a1e
Arnold Siyabonga Mdluli <arnoldmdluli00@gmail.com>
2022-12-03 01:29:46 -0800

    Task 1 fix 4

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-""" script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost',
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         port=3306)
-    """In order to put our new connnection to good use we
-     need to create a cursor object"""
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],port=3306)
     cur = db.cursor()
-    """The execute function requires one parameter, the query."""
-    cur.execute("SELECT * FROM states WHERE name REGEXP BINARY '^N' ORDER BY id ASC")
-    """Obtaining Query Results"""
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    """ Close all cursors"""
-    cur.close()
-    """Close all databases"""
-    db.close()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
