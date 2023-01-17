8a80a3d8db7365d2e2c5d089f6d05d24ccb979e0
Arnold Mdluli <arnoldmdluli00@gmail.com>
2023-01-17 11:34:20 +0000

    Task 0 + 1 fix 4

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file_name = process.argv[2];
-fs.readFile(file_name, 'utf8', function (error, data) {
+const filename = process.argv[2];
+fs.readFile(filename, 'utf8', function (error, data) {
   if (error) {
     return console.log(error);
   }
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file_name = process.argv[2];
-fs.writeFile(file_name, process.argv[3], 'utf8', function (error, data) {
+const filename = process.argv[2];
+fs.writeFile(filename, process.argv[3], 'utf8', function (error, data) {
   if (error) return console.log(error);
 });
