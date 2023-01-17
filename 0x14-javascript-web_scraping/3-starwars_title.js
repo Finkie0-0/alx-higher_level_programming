60dcb5013d252e3132b67734a30497679fb4010a
Arnold Mdluli <arnoldmdluli00@gmail.com>
2023-01-17 12:03:58 +0000

    Task 3 fix 2

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-const filmId = process.argv[2];
-request('https://swapi-api.alx-tools.com/api/films/' + filmId, function (err, response, body) {
+const id = process.argv[2];
+request('https://swapi-api.alx-tools.com/api/films/' + id, function (err, response, body) {
   if (response.statusCode === 200) {
     console.log(JSON.parse(body).title);
   } else {
