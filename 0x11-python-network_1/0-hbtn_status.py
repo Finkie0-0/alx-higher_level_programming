22903832e38d1dbc0f6108e3b5e717cfe3b7f6af
Arnold Mdluli <arnoldmdluli00@gmail.com>
2023-02-08 16:14:11 -0800

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status"""
-
+"""
+Fetches https://alx-intranet.hbtn.io/status
+"""
 
 if __name__ == "__main__":
-    import urllib.request
+    from urllib import request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
+    print('Body response:')
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+    # charset can be gained with response.headers.get_content_charset()
