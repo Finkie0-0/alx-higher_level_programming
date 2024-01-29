31ebc66be71c34f3e4a3a1007e9e86b7c1768f70
Arnold <104798727+Finkie0-0@users.noreply.github.com>
2024-01-29 22:54:40 +0200

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/0-readme.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-const filename = process.argv[2];
-fs.readFile(filename, 'utf8', function (error, data) {
-  if (error) {
-    return console.log(error);
-  }
-  console.log(data);
-});
--- 0x14-javascript-web_scraping/1-writeme.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-const filename = process.argv[2];
-fs.writeFile(filename, process.argv[3], 'utf8', function (error, data) {
-  if (error) return console.log(error);
-});
