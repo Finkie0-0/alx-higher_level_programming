253d5da81118fb413b253a3cfd78ce6c8c18b16f
Arnold <104798727+Finkie0-0@users.noreply.github.com>
2024-01-29 22:54:05 +0200

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// Write a function that returns the reversed version of a list:
-exports.esrever = function (list) {
-  const reversedList = [];
-  for (let index = list.length - 1; index >= 0; index--) {
-    reversedList.push(list[index]);
-  }
-  return reversedList;
-};
